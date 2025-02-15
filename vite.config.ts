e558cca5f151bba87d2a1324a93fa6003d27e7db
sag1v <sagiv.bengiat@gmail.com>
2025-02-15 19:38:39 +0200

    Add Storybook GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import dts from 'vite-plugin-dts';
-import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), dts({ include: ['src'] })],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    dts({ rollupTypes: true })
+  ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+        assetFileNames: 'index.[ext]'
+      }
     },
-  },
+    cssCodeSplit: false,
+    cssMinify: true
+  }
 });
